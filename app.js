918e2463b261b38c0412399601c2afe779969f8d
Ohang <ohangpatel12@gmail.com>
2024-11-15 13:56:21 -0600

    Phase 2(a) - render and delete reviews

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.get("/listings/new", (req, res) => {
 // Show route
 app.get("/listings/:id", wrapAsync (async (req, res) => {
   let { id } = req.params;
-  const listing = await Listing.findById(id);
+  const listing = await Listing.findById(id).populate("reviews");
   res.render("listings/show.ejs", { listing });
 }));
 
@@ -109,7 +109,7 @@ app.delete("/listings/:id", wrapAsync (async (req, res) => {
 
 
 // Reviews
-// Post route
+// Post Review route
 app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
@@ -123,6 +123,15 @@ app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req, res) => {
 
 
 }));
+
+// Delete Review route
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async(req, res) => {
+    let {id, reviewId} = req.params;
+    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+}))
 
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
