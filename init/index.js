f042c463209712c00b7002aebcd9b42328cf0567
Ohang <ohangpatel12@gmail.com>
2024-11-23 12:46:56 -0600

    Assigning listing to Owner

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async() => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({...obj, owner: "6740c55c511b5a447d0be5bc"}));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 }
