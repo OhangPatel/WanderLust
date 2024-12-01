a1ebbcaafb5d67b857bfb0f200b4e0243893924f
Ohang <ohangpatel12@gmail.com>
2024-11-30 20:47:06 -0600

    setting up cloudinary

--- models/listing.js
+++ models/listing.js
@@ -9,10 +9,9 @@ const listingSchema = new Schema({
   },
   description: String,
   image: {
-    type: String,
-    default: "https://images.unsplash.com/photo-1496328488450-9c5c5d555148?q=80&w=2532&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    set: (v) => v === "" ? "https://images.unsplash.com/photo-1496328488450-9c5c5d555148?q=80&w=2532&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
-  },
+    url: String,
+    filename: String,
+},
   price: Number,
   location: String,
   country: String,
--- routes/listing.js
+++ routes/listing.js
@@ -3,17 +3,22 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-
 const listingController = require("../controllers/listings.js");
+const multer  = require('multer')
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
+
 
 router
   .route("/")
   .get(wrapAsync(listingController.index))
   .post(
     isLoggedIn,
+    upload.single('listing[image]'),
     validateListing,
     wrapAsync(listingController.createListing)
   );
+
 
 
 // New route
@@ -25,6 +30,7 @@ router.route("/:id")
 .put(
     isLoggedIn,
     isOwner,
+    upload.single('listing[image]'),
     validateListing,
     wrapAsync(listingController.updateListing)
   )
